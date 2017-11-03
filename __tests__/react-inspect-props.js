4d36e8f61b9df71f415f5db6499502f1e5f9f838
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2017-11-03 20:25:22 -0200

    Initiated tests

---
+++ __tests__/react-inspect-props.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import { inspectProps } from '../src/react-inspect-props'
+
+describe('inspectProps', () => {
+  it('should', () => {
+    expect(true).toBe(true)
+  })
+})
