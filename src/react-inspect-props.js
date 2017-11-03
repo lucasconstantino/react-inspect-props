e48e949fd0b62072de1bc337a0a0960622c687c7
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2017-11-03 20:38:16 -0200

    Allow options to be a function.

--- src/react-inspect-props.js
+++ src/react-inspect-props.js
@@ -19,7 +19,8 @@ export const inspectProps = (name, options = {}) => ComposedComponent => {
     }
 
     componentWillMount () {
-      const connection = window.__REDUX_DEVTOOLS_EXTENSION__.connect({ name, ...options })
+      const config = typeof options === 'function' ? options(this.props) : options
+      const connection = window.__REDUX_DEVTOOLS_EXTENSION__.connect({ name, ...config })
       connection.init(this.props)
       connection.subscribe(this.dispatch)
       this.setState({ connection })
