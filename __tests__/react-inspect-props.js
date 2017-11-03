dc843003a08777ccf5ffe265c2f8ea3617e676bc
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2017-11-03 20:27:03 -0200

    Removed tests

--- __tests__/react-inspect-props.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { mount } from 'enzyme'
-import { inspectProps } from '../src/react-inspect-props'
-
-describe('inspectProps', () => {
-  it('should', () => {
-    expect(true).toBe(true)
-  })
-})
